925a114db2d30d8e0ad5c8e57fc85b0165620e9a
João Lacerda <joao.sclf@gmail.com>
2024-06-24 14:57:32 -0300

    first commit

---
+++ output.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.55.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      owner = "joaolacerda"
+      managed-by = "terraform"
+    }
+  }
+}
